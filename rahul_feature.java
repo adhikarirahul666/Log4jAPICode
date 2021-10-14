d5d5c22199a961482f7490bad92750c5dc98981d
rahul <adhikarirahul666@gmail.com>
2021-10-14 10:23:50 +0530

    testing

--- rahul_feature.java
+++ rahul_feature.java
@@ -3,7 +3,7 @@ public static void main(String[] args) {
     //defining an 'age' variable  
     int age=20;  
     //checking the age  
-    if(age>18){  
+    if(age>8){  
         System.out.print("Age is greater than 18");  
     }  
 }  
